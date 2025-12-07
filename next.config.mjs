57c687b7691a86749e0c4bf027cfab6139a88608
jattu8602 <chaurasiyajatin68@gmail.com>
2025-12-07 19:31:44 +0530

    feat: Add PWA support using Next.js PWA plugin and Workbox.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -11,4 +8,26 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/.*\.basemaps\.cartocdn\.com\/.*/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "map-tiles",
+          expiration: {
+            maxEntries: 500,
+            maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+          },
+        },
+      },
+    ],
+  },
+});
+
+export default withPWA(nextConfig)
